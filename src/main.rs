5c393386bf4736cf4a8b85faf3040d27054ad693
Nathan Teoh <1635687+nathan-teoh@users.noreply.github.com>
2025-05-30 22:05:14 -0700

    Use shared state

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,11 @@
-use axum::{Router, routing::get};
+mod state;
+use std::{
+    collections::HashMap,
+    sync::{Arc, Mutex},
+};
+
+use axum::{Router, extract::State, routing::get};
+use state::Db;
 
 const LISTEN_ADDRESS: &str = "127.0.0.1:8088";
 
@@ -7,8 +14,18 @@ async fn main() {
     // Add a logger
     tracing_subscriber::fmt::init();
 
-    // build app
-    let app = Router::new().route("/", get(root));
+    // Add a shared state
+    let shared_state = Arc::new(Mutex::new(HashMap::<i8, String>::new()));
+
+    // "Seed data"
+    {
+        // Q: How do we drop the lock and allow another reader/writer?
+        let mut db = shared_state.lock().unwrap();
+        db.insert(1, "Hello from db!".to_string());
+    }
+
+    // build app and attach shared state
+    let app = Router::new().route("/", get(root)).with_state(shared_state);
 
     // Create our listener
     let listener = tokio::net::TcpListener::bind(LISTEN_ADDRESS)
@@ -19,6 +36,11 @@ async fn main() {
     axum::serve(listener, app).await.expect("Axum error");
 }
 
-async fn root() -> &'static str {
-    "Hello, SPRUG!"
+// Q: Why did return type change?
+async fn root(State(state): State<Db>) -> String {
+    if let Ok(hashmap) = state.lock() {
+        format!("{:#?}", hashmap.values())
+    } else {
+        "Hello, SPRUG!".to_string()
+    }
 }
