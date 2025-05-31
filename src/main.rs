71c88e425255e747cd211520d0c990258b01b9a6
Nathan Teoh <1635687+nathan-teoh@users.noreply.github.com>
2025-05-30 21:48:50 -0700

    Add axum and simple handler

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, routing::get};
+
+const LISTEN_ADDRESS: &str = "127.0.0.1:8088";
+
+#[tokio::main]
+async fn main() {
+    // Add a logger
+    tracing_subscriber::fmt::init();
+
+    // build app
+    let app = Router::new().route("/", get(root));
+
+    // Create our listener
+    let listener = tokio::net::TcpListener::bind(LISTEN_ADDRESS)
+        .await
+        .expect("Unable to bind");
+
+    // Run it!
+    axum::serve(listener, app).await.expect("Axum error");
+}
+
+async fn root() -> &'static str {
+    "Hello, SPRUG!"
 }
