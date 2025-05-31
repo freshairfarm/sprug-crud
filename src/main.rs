a465c3cad7d186d48085acb011e18ef7c8a3bd2b
Nathan Teoh <1635687+nathan-teoh@users.noreply.github.com>
2025-05-30 23:18:53 -0700

    add new endpoint for todos

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
+mod models;
 mod state;
 use std::{
     collections::HashMap,
     sync::{Arc, Mutex},
 };
 
-use axum::{Router, extract::State, routing::get};
+use axum::{Json, Router, extract::State, routing::get};
 use state::Db;
 
 const LISTEN_ADDRESS: &str = "127.0.0.1:8088";
@@ -15,17 +16,26 @@ async fn main() {
     tracing_subscriber::fmt::init();
 
     // Add a shared state
-    let shared_state = Arc::new(Mutex::new(HashMap::<i8, String>::new()));
+    let shared_state = Arc::new(Mutex::new(HashMap::<i8, models::Todo>::new()));
 
     // "Seed data"
     {
-        // Q: How do we drop the lock and allow another reader/writer?
         let mut db = shared_state.lock().unwrap();
-        db.insert(1, "Hello from db!".to_string());
+        db.insert(
+            1,
+            models::Todo {
+                id: 1,
+                content: "Finish this API!".to_string(),
+                completed: false,
+            },
+        );
     }
 
     // build app and attach shared state
-    let app = Router::new().route("/", get(root)).with_state(shared_state);
+    let app = Router::new()
+        .route("/", get(root))
+        .route("/todo", get(get_todos))
+        .with_state(shared_state);
 
     // Create our listener
     let listener = tokio::net::TcpListener::bind(LISTEN_ADDRESS)
@@ -35,8 +45,14 @@ async fn main() {
     // Run it!
     axum::serve(listener, app).await.expect("Axum error");
 }
-
-// Q: Why did return type change?
+async fn get_todos(State(state): State<Db>) -> Json<Vec<models::Todo>> {
+    if let Ok(hashmap) = state.lock() {
+        let values: Vec<models::Todo> = hashmap.values().cloned().collect();
+        Json(values)
+    } else {
+        Json(Vec::new())
+    }
+}
 async fn root(State(state): State<Db>) -> String {
     if let Ok(hashmap) = state.lock() {
         format!("{:#?}", hashmap.values())
